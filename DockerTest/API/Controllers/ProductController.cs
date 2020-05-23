6c1598e5764e1a13cdf7d6bed748a9475879fce4
Matthias Beerens <3512339+Matthiee@users.noreply.github.com>
2020-05-23 08:53:44 +0200

    Initial commit

---
+++ DockerTest/API/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace API.Controllers
+{
+    public class ProductController : BaseApiController
+    {
+        private readonly ILogger<ProductController> _logger;
+
+        public ProductController(ILogger<ProductController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new Product());
+        }
+    }
+}
